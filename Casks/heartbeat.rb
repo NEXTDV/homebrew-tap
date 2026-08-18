90e600376fa68663e9cccc33a48aa68db8287a4a
Jinwoo Lee <jinlee1703@gmail.com>
2026-08-18 23:17:00 +0900

    heartbeat 0.1.0: tap 릴리즈로 URL 변경

--- Casks/heartbeat.rb
+++ Casks/heartbeat.rb
@@ -2,10 +2,10 @@ cask "heartbeat" do
   version "0.1.0"
   sha256 "2bdde7fae5eb8237a1e7793c9f740f3cf3544d872c2b9e1e8ca87d0fa7bf2806"
 
-  url "https://github.com/NEXTDV/heartbeat-app/releases/download/v#{version}/Heartbeat-#{version}-arm64.dmg"
+  url "https://github.com/NEXTDV/homebrew-tap/releases/download/v#{version}/Heartbeat-#{version}-arm64.dmg"
   name "Heartbeat"
   desc "구독한 서비스 상태를 macOS 메뉴바에서 보는 앱"
-  homepage "https://github.com/NEXTDV/heartbeat-app"
+  homepage "https://github.com/NEXTDV/homebrew-tap"
 
   depends_on arch: :arm64
 
